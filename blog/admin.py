86c84706e02effda2051f155e0961ca5d6444625
Brendan <brendanmadden94@hotmail.com>
2025-11-18 14:28:06 +0000

    Register Comment in admin with admin actions; add Comment migration

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
-# Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'status', 'created_on', 'updated_on')
+	list_filter = ('status', 'created_on', 'author')
+	search_fields = ('title', 'content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('__str__', 'author', 'post', 'approved', 'created_on')
+	list_filter = ('approved', 'created_on', 'author')
+	search_fields = ('body', 'author__username', 'post__title')
+	actions = ['approve_comments']
+
+	def approve_comments(self, request, queryset):
+		queryset.update(approved=True)
+
+	approve_comments.short_description = "Mark selected comments as approved"
