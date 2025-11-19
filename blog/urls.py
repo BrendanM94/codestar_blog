7762d6252c21a418bdb7590e49c25a94c82c67c8
Brendan <brendanmadden94@hotmail.com>
2025-11-19 15:10:55 +0000

    Add post_detail function view, URL route, template, and link from index

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
     path('', views.PostList.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -9,3 +10,9 @@ class PostList(generic.ListView):
     template_name = 'blog/index.html'
     context_object_name = 'posts'
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """Function-based view to display a single published post by slug."""
+    post = get_object_or_404(Post, slug=slug, status=1)
+    return render(request, 'blog/post_detail.html', {'post': post})
